9c4e13794f159ca9e2ddb304bb506f0f1c1efc83
hazem <hazem.yahyawi159@gmail.com>
2023-01-17 20:29:41 +0100

    secondcommit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Header from "../components/header/Header"
 import { Feautured } from "../components/featured/Featured";
 import { PropretyList } from "../components/PropretyList/PropretyList";
 import { FeaturedProperties } from "../components/FeaturedProperties/FeaturedProperties";
+import { MailList } from "../components/MailList/MailList";
+import { Footer } from "../components/foooter/Footer";
 import "./Home.css";
 export const Home = () => {
     return (<>
@@ -11,6 +13,8 @@ export const Home = () => {
         <div className="homeContainer"><Feautured />
             <h1 className="homeTitle">Browse by property type</h1><PropretyList />
             <h1 className="homeTitle">Homes guests love</h1><FeaturedProperties/>
+            <MailList/>
+            <Footer/>
             </div>
             
 
